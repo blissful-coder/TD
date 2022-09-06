53424d1cb6c228766885582fb2eceeeeac30041b
blissful_coder <anurag.negi1729@gmail.com>
2022-09-06 19:48:45 +0530

    feat(working): added working version

--- preload.js
+++ preload.js
@@ -1,5 +1,8 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
+
+
+
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
@@ -10,3 +13,5 @@ window.addEventListener('DOMContentLoaded', () => {
     replaceText(`${type}-version`, process.versions[type])
   }
 })
+
+
